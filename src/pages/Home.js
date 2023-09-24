fd72cd93368462d44f14e05cdfb6461efcbfd2b5
Hanumant Jain <hanumant@Hanumants-MacBook-Pro.local>
2023-09-23 23:39:19 -0400

    nav,message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import TheWorld from './TheWorld'
 import Message from './Message'
 import Profile from './Profile'
 import Settings from './Settings'
@@ -9,19 +8,21 @@ import Settings from './Settings'
 
 const Home = () => {
   return (
-    <div>
+    <>
+    <div className=''>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/TheWorld' Component={TheWorld} />
+          <Route path='/Home' Component={Home} />
           <Route path='/Message' Component={Message} />
           <Route path='/Profile' Component={Profile} />
           <Route path='/Settings' Component={Settings} />
         </Routes>
-
+        
       </BrowserRouter>
-      
     </div>
+    </>
+    
   )
 }
 
--- src/pages/TheWorld.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TheWorld = () => {
-  return (
-    <div>TheWorld</div>
-  )
-}
-
-export default TheWorld
